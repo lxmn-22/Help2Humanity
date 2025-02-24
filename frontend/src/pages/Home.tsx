5c6cb1ac50e120ca0c6b49f5accc83faa73f0e58
Laxman Singh Koranga <korangalaxman03@gmail.com>
2025-02-24 21:02:44 +0530

    [Help2Humanity]: Completed Registration Form.

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Mission from "../components/Mission";
+
+const Home = () => {
+	return (
+		<div>
+			<Hero />
+			<Mission />
+		</div>
+	);
+};
+
+export default Home;
